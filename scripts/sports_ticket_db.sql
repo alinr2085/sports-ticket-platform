4e0d67a4d0da734075316f461334fbda2cc4a57f
AmirReza <Amireza0627safari@gmail.com>
2026-08-16 11:19:15 +0330

    creating ticket category, ticket, ticket details, user tables

--- scripts/sports_ticket_db.sql
+++ scripts/sports_ticket_db.sql
@@ -60,3 +60,53 @@ CREATE TABLE MatchTable (
         (leagueId IS NULL AND tournamentId IS NOT NULL)
     )
 );
+
+
+CREATE TABLE TicketCategory (
+    ticketCategoryId INT AUTO_INCREMENT PRIMARY KEY,
+    ticketCategoryName VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL CHECK (price >= 0),
+    remainingCapacity INT NOT NULL,
+    matchId INT NOT NULL,
+    FOREIGN KEY (matchId) REFERENCES MatchTable(matchId)
+);
+
+CREATE TABLE User (
+    userId INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    phoneNumber VARCHAR(20) UNIQUE,
+    hashedPassword VARCHAR(255) NOT NULL,
+    cityId INT,
+    dateOfBirth DATE,
+    registrationDate DATE NOT NULL DEFAULT (CURRENT_DATE),
+    accountStatus VARCHAR(50) NOT NULL DEFAULT 'ACTIVE',
+    role VARCHAR(20) NOT NULL DEFAULT 'user',
+    FOREIGN KEY (cityId) REFERENCES City(cityId),
+    CHECK (email IS NOT NULL OR phoneNumber IS NOT NULL),
+    CHECK (role IN ('user', 'support'))
+);
+
+CREATE TABLE Ticket (
+    ticketId INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    sectionNumber INT,
+    status VARCHAR(50) NOT NULL DEFAULT 'available',
+    ticketCategoryId INT NOT NULL,
+    matchId INT NOT NULL,
+    FOREIGN KEY (ticketCategoryId) REFERENCES TicketCategory(ticketCategoryId),
+    FOREIGN KEY (matchId) REFERENCES MatchTable(matchId),
+  FOREIGN KEY (userId) REFERENCES User(userId)
+
+);
+
+CREATE TABLE TicketDetails (
+    ticketId INT PRIMARY KEY,
+    tournamentName VARCHAR(255),
+    leagueName VARCHAR(255),
+    stadiumName VARCHAR(255),
+    facilities VARCHAR(2047),
+    FOREIGN KEY (ticketId) REFERENCES Ticket(ticketId)
+);
+
